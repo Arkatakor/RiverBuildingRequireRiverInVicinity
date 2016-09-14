b36fd29fdc78c1aa94391bd95bfc8e8570faa855
Arkatakor <zakariel76@gmail.com>
2016-09-14 16:35:19 +0200

    Putting in lake checker for gardens and floating gardens

--- RiverBuildingRequireRiverInVicinity/BuildingsRiverVicinity.lua
+++ RiverBuildingRequireRiverInVicinity/BuildingsRiverVicinity.lua
@@ -3,18 +3,19 @@
 -- DateCreated: 10-Sep-16 8:20:58 AM
 --------------------------------------------------------------
 local tBuildingRequireNearbyRiver = {}
-for row in GameInfo.Building_RequireNearbyRiverOrLake() do
+local tBuildingRequireNearbyLake = {}
+
+for row in GameInfo.Building_RequireNearbyRiver() do
 	tBuildingRequireNearbyRiver[GameInfoTypes[row.BuildingType]] = true
-	--TODO: figure this out; post in forums; look up lua tables
-	--table.insert(tBuildingRequireNearbyRiver[GameInfoTypes[row.BuildingType]], GameInfoTypes[row.FreshWater])
+	tBuildingRequireNearbyLake[GameInfoTypes[row.BuildingType]] = row.FreshWater		
 end
-function CityHasRiver(pCity)
+function CityHasRiverOrLake(pCity, bFreshWater)
 	local iNumPlots = pCity:GetNumCityPlots()
 	local iPlayer = pCity:GetOwner()
 	for i = 0, iNumPlots - 1 do
 		local pPlot = pCity:GetCityIndexPlot(i)
 		if pPlot and (pPlot:GetOwner() == iPlayer) then
-			if  pPlot:IsRiver() then
+			if  pPlot:IsRiver() or (bFreshWater == true and pPlot:IsLake()) then			
 				return true
 			end
 		end
@@ -24,12 +25,10 @@ end
 function CanConstructRiverBuilding(PlayerID, CityID, BuildingType)
 	if tBuildingRequireNearbyRiver[BuildingType] then
 		local pPlayer = Players[PlayerID]
+		local bFreshWater = tBuildingRequireNearbyLake[BuildingType]
 		local pCity = pPlayer:GetCityByID(CityID)
-		return CityHasRiver(pCity)
+		return CityHasRiverOrLake(pCity, bFreshWater)
 	end
 	return true
 end 
 GameEvents.CityCanConstruct.Add(CanConstructRiverBuilding);
-
-
-
